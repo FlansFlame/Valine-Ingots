b211b38d1879c9c23d297daab325357a511fc2a2
FlansFlame <alfas8139@gmail.com>
2025-09-27 16:56:01 +0900

    add armors

---
+++ src/main/java/net/flansflame/varin_ingots/world/item/ModArmorMaterials.java
@@ -0,0 +1,80 @@
+package net.flansflame.varin_ingots.world.item;
+
+import net.flansflame.varin_ingots.VarinIngots;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.crafting.Ingredient;
+
+import java.util.function.Supplier;
+
+public enum ModArmorMaterials implements ArmorMaterial {
+    VARIN("varin", 1, 24, 25,
+            SoundEvents.ARMOR_EQUIP_NETHERITE, 2f, 0f, () -> Ingredient.of(ModItems.VARIN_INGOT.get()));
+
+    private final String name;
+    private final int durabilityMultiplier;
+    private final int[] protectionAmounts;
+    private final int enchantmentValue;
+    private final SoundEvent equipSound;
+    private final float toughness;
+    private final float knockbackResistance;
+    private final Supplier<Ingredient> repairIngredient;
+
+    private static final int[] BASE_DURABILITY = {1024, 2048, 2048, 1024};
+
+    ModArmorMaterials(String name, int durabilityMultiplier, int protectionAmount, int enchantmentValue, SoundEvent equipSound,
+                      float toughness, float knockbackResistance, Supplier<Ingredient> repairIngredient) {
+        this.name = name;
+        this.durabilityMultiplier = durabilityMultiplier;
+        this.protectionAmounts = new int[]{
+                protectionAmount - 5, protectionAmount, protectionAmount - 2, protectionAmount - 3
+        };
+        this.enchantmentValue = enchantmentValue;
+        this.equipSound = equipSound;
+        this.toughness = toughness;
+        this.knockbackResistance = knockbackResistance;
+        this.repairIngredient = repairIngredient;
+    }
+
+    @Override
+    public int getDurabilityForType(ArmorItem.Type pType) {
+        return BASE_DURABILITY[pType.ordinal()] * this.durabilityMultiplier;
+    }
+
+    @Override
+    public int getDefenseForType(ArmorItem.Type pType) {
+        return this.protectionAmounts[pType.ordinal()];
+    }
+
+    @Override
+    public int getEnchantmentValue() {
+        return enchantmentValue;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return this.equipSound;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredient.get();
+    }
+
+    @Override
+    public String getName() {
+        return VarinIngots.MOD_ID + ":" + this.name;
+    }
+
+    @Override
+    public float getToughness() {
+        return this.toughness;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return this.knockbackResistance;
+    }
+}
--- src/main/java/net/flansflame/varin_ingots/world/item/ModItems.java
+++ src/main/java/net/flansflame/varin_ingots/world/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.flansflame.varin_ingots.world.item;
 
 import net.flansflame.varin_ingots.VarinIngots;
+import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -14,6 +15,15 @@ public class ModItems {
 
     public static final RegistryObject<Item> VARIN_INGOT = ITEMS.register("varin_ingot",
             () -> new Item(new Item.Properties().rarity(Rarity.RARE).fireResistant()));
+
+    public static final RegistryObject<Item> VARIN_HELMET = ITEMS.register("varin_helmet",
+            () -> new ArmorItem(ModArmorMaterials.VARIN, ArmorItem.Type.HELMET, new Item.Properties().rarity(Rarity.RARE)));
+    public static final RegistryObject<Item> VARIN_CHESTPLATE = ITEMS.register("varin_chestplate",
+            () -> new ArmorItem(ModArmorMaterials.VARIN, ArmorItem.Type.CHESTPLATE, new Item.Properties().rarity(Rarity.RARE)));
+    public static final RegistryObject<Item> VARIN_LEGGINGS = ITEMS.register("varin_leggings",
+            () -> new ArmorItem(ModArmorMaterials.VARIN, ArmorItem.Type.LEGGINGS, new Item.Properties().rarity(Rarity.RARE)));
+    public static final RegistryObject<Item> VARIN_BOOTS = ITEMS.register("varin_boots",
+            () -> new ArmorItem(ModArmorMaterials.VARIN, ArmorItem.Type.BOOTS, new Item.Properties().rarity(Rarity.RARE)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
